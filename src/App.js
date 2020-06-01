1fa89af31dd507510ced55c3454919d09f10ee9e
Lester <lester.deguzman@tup.edu.ph>
2020-06-01 10:30:45 +0800

    Initialize Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Home from "./containers/Home/Home";
+import Auth from "./containers/Auth/Auth";
+import CreatePost from "./containers/CreatePost/CreatePost";
+import Post from "./containers/Post/Post";
+import User from "./containers/User/User";
 
 function App() {
+  console.log("App rendered");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <Auth />
+      <CreatePost />
+      <Post />
+      <User />
     </div>
   );
 }
